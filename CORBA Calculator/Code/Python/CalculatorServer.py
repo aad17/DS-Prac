dc2377c925074cf0e4ae2f0479452171a7df5607
aad17 <65403228+aad17@users.noreply.github.com>
2023-05-23 20:35:14 +0530

    Add files via upload

--- CORBA Calculator/Code/Python/CalculatorServer.py
+++ CORBA Calculator/Code/Python/CalculatorServer.py
@@ -21,3 +21,10 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# If error on NameServer
+# Step 1: Open a new terminal
+# Step 2: python -m Pyro4.naming (keep it running)
+# Step 3: Open a new terminal
+# Step 4: python CalculatorServer.py
